d9cda90952f130b2f72a13c94ddefd480c3580e5
Andreas Carlsson <ante84@gmail.com>
2017-01-25 21:40:40 +0100

    New ads controller class.

---
+++ Login FX/src/com/Ads/AdsController.java
@@ -0,0 +1,58 @@
+package com.Ads;
+
+import database.Database;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+import java.awt.*;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Ante on 2017-01-25.
+ */
+public class AdsController implements Initializable {
+
+    private Database db;
+
+    @FXML
+    private Button btnPrev;
+    @FXML
+    private Button btnNext;
+    @FXML
+    private ImageView image;
+    @FXML
+    private Button btnUpload;
+
+    public AdsController() {
+        try {
+            db = new Database();
+        }
+        catch(SQLException e) {
+            System.out.println("SQL error in AdsController.");
+        }
+    }
+
+    @FXML
+    private void prev(ActionEvent a) {
+        System.out.println("Previous");
+    }
+
+    @FXML
+    private void next(ActionEvent a) {
+        System.out.println("Next");
+    }
+
+    @FXML
+    private void upload(ActionEvent a) {
+        System.out.println("Upload");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+}
